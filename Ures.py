5948c8ec223e1cb1929dda711a87640436100eb7
Oghux <barna.gyurasits@gmail.com>
2026-05-18 19:59:55 +0200

    percmutato kesz

--- Ures.py
+++ Ures.py
@@ -5,4 +5,18 @@ turtle.color("red")
 turtle.setheading(20)
 turtle.forward(100)
 
+turtle.goto(0,0)
+turtle.pensize(2)
+turtle.color("yellow")
+turtle.setheading(120)
+turtle.forward(100)
+
+
+
+turtle.goto(0,0)
+turtle.width(5)
+turtle.setheading(60)
+turtle.color("black")
+turtle.forward(100)
+
 turtle.done()
