bead19902429ae3dd0c3d87d2d004c6234556bb6
MakaroniXD <macivagyokhehe@gmail.com>
2026-05-18 20:07:20 +0200

    feladat kesz

--- Ures.py
+++ Ures.py
@@ -1,15 +1,20 @@
 import turtle
 
+turtle.pensize(5)
 turtle.color("red")
 
 turtle.setheading(20)
-turtle.forward(100)
+turtle.forward(120)
+turtle.forward(-120)
 
 turtle.goto(0,0)
-turtle.pensize(2)
+turtle.pensize(3)
+
 turtle.color("yellow")
 turtle.setheading(120)
 turtle.forward(100)
+turtle.forward(-100)
+
 
 
 
@@ -17,6 +22,6 @@ turtle.goto(0,0)
 turtle.width(5)
 turtle.setheading(60)
 turtle.color("black")
-turtle.forward(100)
+turtle.forward(150)
 
 turtle.done()
